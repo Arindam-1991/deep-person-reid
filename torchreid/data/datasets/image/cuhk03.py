10b29484032c68866031f16bafa4f7502183f84e
purbayankar <53092711+purbayankar@users.noreply.github.com>
2020-12-27 22:24:11 +0530

    Update cuhk03.py

--- torchreid/data/datasets/image/cuhk03.py
+++ torchreid/data/datasets/image/cuhk03.py
@@ -21,11 +21,11 @@ class CUHK03(ImageDataset):
         - splits: 20 (classic).
     """
     dataset_dir = 'cuhk03'
-    dataset_url = None
+    dataset_url = 'https://drive.google.com/u/0/uc?id=0BxJeH3p7Ln48djNVVVJtUXh6bXc&export=download'
 
     def __init__(
         self,
-        root='/content/drive/MyDrive/cuhk03_release/',
+        root='',
         split_id=0,
         cuhk03_labeled=False,
         cuhk03_classic_split=False,
