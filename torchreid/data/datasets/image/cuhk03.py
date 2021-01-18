6188dc28b7810c02147b946dd2a1c8d7dd1f22c9
purbayankar <53092711+purbayankar@users.noreply.github.com>
2021-01-19 01:43:27 +0530

    Update cuhk03.py

--- torchreid/data/datasets/image/cuhk03.py
+++ torchreid/data/datasets/image/cuhk03.py
@@ -25,7 +25,7 @@ class CUHK03(ImageDataset):
 
     def __init__(
         self,
-        root='/content/drive/Shareddrives/SiameseViT',
+        root='',
         split_id=0,
         cuhk03_labeled=False,
         cuhk03_classic_split=False,
