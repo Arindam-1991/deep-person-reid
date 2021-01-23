5b7624705236e1d3c5e65046fc7d7163d43f6d99
Arindam Sikdar <as26@iitbbs.ac.in>
2021-01-23 13:51:41 +0530

    Update visualTransformer.py

--- torchreid/models/visualTransformer.py
+++ torchreid/models/visualTransformer.py
@@ -93,8 +93,9 @@ class ViT(nn.Module):
         channels = 3, 
         dim_head = 64, 
         dropout = 0., 
-        emb_dropout = 0., 
-        loss = 'xent'):
+        emb_dropout = 0.,
+        loss = 'softmax',
+        **kwargs):
 
         super().__init__()
         assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
